a87abe1dfa74623decaad162dc2f99bd02b4980c
omarsalim <83906211+Everezze@users.noreply.github.com>
2023-11-26 17:09:21 +0100

    share button fb/bg colors added, shadow on socials added

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const shareButton = document.querySelector(".share-button");
 const personInfos= document.querySelector(".infos");
 const personSocials= document.querySelector(".socials");
 const asideContainer = document.querySelector("aside");
-console.log(asideContainer);
+const arrowSvg = document.querySelector(".share-button path");
+console.log(arrowSvg);
 
 shareButton.addEventListener("click",function(){
+    this.classList.toggle("bg");
     personInfos.classList.toggle("remove");
     personSocials.classList.toggle("add");
     asideContainer.classList.toggle("active-socials");
+    arrowSvg.classList.toggle("active");
     console.log(window.innerWidth);
 
     if(window.innerWidth >=819){
